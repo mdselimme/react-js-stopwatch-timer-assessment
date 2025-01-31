1fee6456b4767d657f728faacea7808984c9f95c
mdselimme <mdselimdev@gmail.com>
2025-01-31 18:13:57 +0600

    add-timer-full-ui-design

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import StopWatch from "./assets/Pages/StopWatch/StopWatch";
 function App() {
   return (
     <div className="main-body">
+      {/* stop watch main components page import  */}
       <StopWatch></StopWatch>
     </div>
   );
--- src/assets/Pages/StopWatch/StopWatch.jsx
+++ src/assets/Pages/StopWatch/StopWatch.jsx
@@ -1,27 +1,56 @@
 import "./StopWatch.css";
+import { useState } from "react";
+import { IoMdPause, IoMdPlay } from "react-icons/io";
 const StopWatch = () => {
+  const [btnToggle, setBtnToggle] = useState(true);
+
+  // Start Timer Function
+  const startTimer = () => {
+    setBtnToggle(false);
+    console.log("start timer");
+  };
+
+  // Stop Timer Function
+  const stopTimer = () => {
+    setBtnToggle(true);
+    console.log("stop timer");
+  };
+
+  // Reset Timer Function
+  const resetTimer = () => {
+    console.log("reset timer");
+  };
+
   return (
     <div className="stopwatch-container">
       <h1>Stopwatch Timer</h1>
-      <div className="timer-box">
-        <div className="times-number">
-          <p>00</p>
-          <span>Hours</span>
+      <div className="timer-show-box">
+        <div className="timer-box">
+          <div className="times-number">
+            <h1>
+              00 <span>:</span> 00 <span>:</span> 00 <span>:</span> 00
+            </h1>
+          </div>
         </div>
-        <div>
-          <p>:</p>
+        {/* Start and Pause Timer Button Start */}
+        <div className="play-pause-btn">
+          {btnToggle ? (
+            <button type="button" title="Start timer" onClick={startTimer}>
+              <IoMdPlay />
+            </button>
+          ) : (
+            <button type="button" title="Pause timer" onClick={stopTimer}>
+              <IoMdPause />
+            </button>
+          )}
         </div>
-        <div className="times-number">
-          <p>00</p>
-          <p>Minutes</p>
-        </div>
-        <div>
-          <p>:</p>
-        </div>
-        <div className="times-number">
-          <p>00</p>
-          <p>Seconds</p>
-        </div>
+        {/* Start and Pause Timer Button Start */}
+      </div>
+      {/* Reset Timer Button  */}
+      <div className="reset-timer">
+        <button onClick={resetTimer} title="Reset Timer" type="button">
+          Reset Timer
+        </button>
       </div>
     </div>
   );
